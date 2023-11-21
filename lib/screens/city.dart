b8e7b86f5a2274b1ea35568148055ce94ed28709
CarlosEduardoMarquesdeAlmeida <carlose.maciel@unifacol.edu.br>
2023-11-21 16:03:32 -0300

    Refactor UI layout in City screen

--- lib/screens/city.dart
+++ lib/screens/city.dart
@@ -12,15 +12,14 @@ class _CityState extends State<City> {
   Widget build(BuildContext context){
     
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 45, 0, 170),
       body: Container(
-
       child: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
           children: <Widget>[
-            Spacer(),
             CityCard(),
             WetherCard(),
-            Spacer(),
           ]
         ),
       ),
@@ -31,6 +30,7 @@ class _CityState extends State<City> {
  
   }
 }
+
 
 class CityCard extends StatefulWidget{
   const CityCard({super.key});
@@ -45,14 +45,16 @@ class _CityCardState extends State<CityCard> {
 
     return Center(
       child: Card(
-        child: SizedBox(
-
-          width: 680,
-          height: 430,
+        color: Color.fromARGB(255, 103, 76, 186),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 60.0, 20.0, 60),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
             children:[Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              
               Construtor_cidade("Thunderstorm", 56),
               Icon(Icons.star_border_outlined)
             ],
@@ -62,14 +64,24 @@ class _CityCardState extends State<CityCard> {
             
           ),
           Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text("uareizero"),
-              Text("CityCountry"),
+              
+              Text("Juareizero",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              )),
+              Text("CityCountry"
+              ,style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              )),
               //Cityname vai falar o nome da cidade
             ],
           ), 
            Row(
+          
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Construtor_clima("Thunderstorm", "00:00"),
@@ -107,8 +119,6 @@ class _CityCardState extends State<CityCard> {
  Widget Construtor_clima(String Clima, String horario){
     return Column(
       children: [
-        Row(
-          children:[
             if (Clima == "Thunderstorm") Icon(Icons.thunderstorm_rounded),
             if (Clima == "Drizzle") Icon(Icons.foggy),
             if (Clima == "Rain") Icon(Icons.cloudy_snowing), 
@@ -116,10 +126,8 @@ class _CityCardState extends State<CityCard> {
             if (Clima == "Clear") Icon(Icons.wb_sunny), 
             if (Clima == "Clouds") Icon(Icons.cloud),
             Text(horario),
-          ],
-        )
-    ],
-    );
+      ],
+        );
   }
 class WetherCard extends StatefulWidget{
   const WetherCard({super.key});
@@ -134,29 +142,45 @@ class _WetherCardState extends State<WetherCard>{
   Widget build(BuildContext context) {
     return Center(
       child: Card(
+        color: Color.fromARGB(255, 103, 76, 186),
+          child: Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 60.0, 20.0, 60),
         child: SizedBox(
-          width: 680,
-          height: 589,
-          child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          width: 350,
+          height: 250,
+          child: Column(
+            
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.min,
+            
             children: [
+              Spacer(),
               Construtor_Semanal("16/08/23", 56, "Thunderstorm", 56),
+              Spacer(),
               Construtor_Semanal("16/08/23", 56, "Thunderstorm", 56),
+              Spacer(),
               Construtor_Semanal("16/08/23", 56, "Thunderstorm", 56),
+              Spacer(),
               Construtor_Semanal("16/08/23", 56, "Thunderstorm", 56),
+              Spacer(),
               Construtor_Semanal("16/08/23", 56, "Thunderstorm", 56),
+              Spacer(),
               ],
               )
           )
         ),
+      ),
       );
   
 
   }
   Widget Construtor_Semanal(String Dia_da_Semana, int Umidade, String Clima, double Temperatura){
-
+    
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
       children: [
+
         Text(Dia_da_Semana),
         Row(
           children: [
@@ -180,7 +204,25 @@ class _WetherCardState extends State<WetherCard>{
   }
 
 }
+class FavoriteButton extends StatefulWidget{
+  const FavoriteButton({super.key});
 
+  @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool standerSelect = false;
+  @override
+  Widget build(BuildContext context){
+    return IconButton(
+      isSelected: ,
+      icon: Icon(Icons.star_border_outlined),
+
+      onPressed: (){},
+    );
+  }
+}
 
 //uma classe chamada city card
 //outra classe chamada wether card 
